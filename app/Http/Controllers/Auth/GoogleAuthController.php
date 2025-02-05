863edf2cf9d39c81d0938ac8e21dc9a23fda6839
aprnna <aprianacahya04@gmail.com>
2025-02-06 00:11:00 +0700

    fix: google sign in

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -20,15 +20,23 @@ class GoogleAuthController extends Controller
     {
         $userGoogle = Socialite::driver('google')->user();
 
-        $user = User::updateOrCreate(
-            ['google_id' => $userGoogle->id],
-            [
+        $user = User::where('google_id', $userGoogle->id)->orWhere('email', $userGoogle->email)->first();
+
+        if ($user) {
+            // User already exists, log them in
+            $user->update(['google_id' => $userGoogle->id]);
+            Auth::login($user);
+            return redirect()->intended(route('home.index', absolute: false));
+        } else {
+            // User does not exist, create a new user
+            $user = User::create([
                 'name' => $userGoogle->name,
                 'email' => $userGoogle->email,
-                'password' => Str::password(12),
-            ]
-        );
-        Auth::login($user);
-        return redirect()->intended(route('home.index', absolute: false));
+                'google_id' => $userGoogle->id,
+                'password' => Str::random(12),
+            ]);
+            Auth::login($user);
+            return redirect()->intended(route('user-detail.create', absolute: false));
+        }
     }
 }
